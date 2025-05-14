c86e510eb6a861c770a10b64f47ba89d048a35a2
william-wv <wwillveiga@hotmail.com>
2025-05-14 05:19:47 -0300

    header project

---
+++ components/Header.php
@@ -0,0 +1,14 @@
+<nav class=" mx-auto max-w-screen-lg flex justify-between px-8 py-4">
+  <div class="font-bold text-xl tracking-wide">
+    Book Wise
+  </div> 
+  <ul class="flex space-x-4 jont-bold">
+    <li><a class="text-blue-600" href="/">Explorar</a></li>
+    <li><a class="hover:underline" href="/meus-livros.">Meus livros</a></li>
+  </ul>
+  <ul>
+    <li>
+      <a class="hover:underline" href="/login.php">Fazer login</a>
+    </li>
+  </ul>
+</nav>
--- index.php
+++ index.php
@@ -1,5 +1,26 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
 
-echo 'hello Word';
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
+  <title>Book Wise</title>
+</head>
 
-?>
+<body class="bg-stone-950 text-stone-200">
+  <header class="bg-stone-900">
+  <?php include('./components/Header.php') ?>
+  </header>
+
+  <main>
+  <?php include('./components/Main.php') ?>
+
+  </main>
+
+  <footer>
+
+  </footer>
+</body>
+
+</html>
